f3723fa9ea7290c108b10863fe81514e644b6f02
ahmed <76961547+ahmedsaad562000@users.noreply.github.com>
2022-10-13 19:38:38 +0200

    Revert "first commit"
    This reverts commit 7861212e9ad60a2535c8d599a72e38a79d1a3a16.

--- E_commerce_app/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.e_commerce_app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.e_commerce_app to javafx.fxml;
-    exports com.example.e_commerce_app;
-}
