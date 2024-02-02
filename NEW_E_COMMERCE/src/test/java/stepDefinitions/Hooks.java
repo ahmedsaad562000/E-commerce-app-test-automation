63c6a063f302cfc0b706fadac8302d5a1d0d294a
DESKTOP-M5TOENG\AHMED <ahmed.moh.saad.hussein.ali@gmail.com>
2024-02-02 08:58:37 +0200

    ✅ update chromedriver

--- NEW_E_COMMERCE/src/test/java/stepDefinitions/Hooks.java
+++ NEW_E_COMMERCE/src/test/java/stepDefinitions/Hooks.java
@@ -4,6 +4,8 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.concurrent.TimeUnit;
 public class Hooks {
     public static WebDriver driver;
@@ -14,9 +16,11 @@ public class Hooks {
         String chromepath = System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe";
 
         System.setProperty("webdriver.chrome.driver", chromepath);
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-blink-features=AutomationControlled");
+        options.addArguments("--remote-allow-origins=*");
         //2-new object
-        driver = new ChromeDriver(); //acts as mouse and keyboard
+        driver = new ChromeDriver(options); //acts as mouse and keyboard
         //3- Configurations
         driver.manage().window().maximize();
       //  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
